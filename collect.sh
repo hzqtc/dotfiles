bf417dc24e4dd23c2c7bd178089a303811b3b16e
Johnny Huang <hzqtc1229@gmail.com>
2012-03-13 12:25:28 +0800

    improve collect and distribute scripts

--- collect.sh
+++ collect.sh
@@ -1,62 +1,18 @@
 #!/bin/bash
 
-if [ -f ~/.vimrc ]
-then
-	echo 'Collecting vimrc'
-	cp ~/.vimrc vimrc
-fi
+dotfiles=( ~/.vimrc ~/.zshrc ~/.aliasrc ~/.fonts.conf ~/.gitconfig ~/.xbindkeysrc ~/.config/zathura/zathurarc ~/.vimperatorrc ~/.vimperator/info/default/quickmarks )
 
-if [ -f ~/.zshrc ]
-then
-	echo 'Collecting zshrc'
-	cp ~/.zshrc zshrc
-fi
-
-if [ -f ~/.aliasrc ]
-then
-	echo 'Collecting aliasrc'
-	cp ~/.aliasrc aliasrc
-fi
-
-if [ -f ~/.fonts.conf ]
-then
-	echo 'Collecting fonts.conf'
-	cp ~/.fonts.conf fonts.conf
-fi
-
-if [ -f ~/.tmux.conf ]
-then
-	echo 'Collecting tmux.conf'
-	cp ~/.tmux.conf tmux.conf
-fi
-
-if [ -f ~/.gitconfig ]
-then
-	echo 'Collecting gitconfig'
-	cp ~/.gitconfig gitconfig
-fi
-
-if [ -f ~/.xbindkeysrc ]
-then
-	echo 'Collecting xbindkeysrc'
-	cp ~/.xbindkeysrc xbindkeysrc
-fi
-
-if [ -f ~/.config/zathura/zathurarc ]
-then
-	echo 'Collecting zathurarc'
-	cp ~/.config/zathura/zathurarc zathurarc
-fi
-
-if [ -f ~/.vimperatorrc ]
-then
-    echo 'Collecting vimperatorrc'
-    cp ~/.vimperatorrc vimperatorrc
-fi
-
-if [ -f ~/.vimperator/info/default/quickmarks ]
-then
-    echo 'Collecting vimperator quickmarks'
-    cp ~/.vimperator/info/default/quickmarks vimperator-quickmarks
-fi
+for file in ${dotfiles[*]}
+do
+    if [ -f $file ]
+    then
+        name=${file##*/}
+        if [ `expr index "$name" "."` == "1" ]
+        then
+            name=${name#.}
+        fi
+        echo "$file => $name"
+        cp "$file" "$name"
+    fi
+done
 
--- distribute.sh
+++ distribute.sh
@@ -1,62 +1,27 @@
 #!/bin/bash
 
-if [ -f ~/.vimrc ]
-then
-	echo 'Distributing vimrc'
-	cp vimrc ~/.vimrc
-fi
+dotfiles=( ~/.vimrc ~/.zshrc ~/.aliasrc ~/.fonts.conf ~/.gitconfig ~/.xbindkeysrc ~/.config/zathura/zathurarc ~/.vimperatorrc ~/.vimperator/info/default/quickmarks )
 
-if [ -f ~/.zshrc ]
-then
-	echo 'Distributing zshrc'
-	cp zshrc ~/.zshrc
-fi
+for file in ${dotfiles[*]}
+do
+    name=${file##*/}
+    if [ `expr index "$name" "."` == "1" ]
+    then
+        name=${name#.}
+    fi
 
-if [ -f ~/.aliasrc ]
-then
-	echo 'Distributing aliasrc'
-	cp aliasrc ~/.aliasrc
-fi
-
-if [ -f ~/.fonts.conf ]
-then
-	echo 'Distributing fonts.conf'
-	cp fonts.conf ~/.fonts.conf
-fi
-
-if [ -f ~/.tmux.conf ]
-then
-	echo 'Distributing tmux.conf'
-	cp tmux.conf ~/.tmux.conf
-fi
-
-if [ -f ~/.gitconfig ]
-then
-	echo 'Distributing gitconfig'
-	cp gitconfig ~/.gitconfig
-fi
-
-if [ -f ~/.xbindkeysrc ]
-then
-	echo 'Distributing xbindkeysrc'
-	cp xbindkeysrc ~/.xbindkeysrc
-fi
-
-if [ -f ~/.config/zathura/zathurarc ]
-then
-	echo 'Distributing zathurarc'
-	cp zathurarc ~/.config/zathura/zathurarc
-fi
-
-if [ -f ~/.vimperatorrc ]
-then
-    echo 'Distributing vimperatorrc'
-    cp vimperatorrc ~/.vimperatorrc
-fi
-
-if [ -f ~/.vimperator/info/default/quickmarks ]
-then
-    echo 'Distributing vimperator quickmarks'
-    cp vimperator-quickmarks ~/.vimperator/info/default/quickmarks
-fi
+    if [ -f $file ]
+    then
+        echo "$name => $file"
+        cp "$name" "$file"
+    else
+        echo -n "Do you wish to copy $name to $file? [Yn]"
+        read ans
+        if [ "$ans" == "" -o "$ans" == "y" -o "$ans" == "Y" ]
+        then
+            echo "$name => $file"
+            cp "$name" "$file"
+        fi
+    fi
+done
 
