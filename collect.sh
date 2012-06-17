be7ca58f5c60f5d9a2f394fb7ccdda1c08526fb4
Johnny Huang <hzqtc1229@gmail.com>
2012-06-17 22:40:28 +0800

    add firefox userchrome.css and usercontent.css; remove zathurarc

--- collect.sh
+++ collect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dotfiles=( ~/.vimrc ~/.zshrc ~/.aliasrc ~/.fonts.conf ~/.gitconfig ~/.xbindkeysrc ~/.config/zathura/zathurarc ~/.vimperatorrc ~/.vimperator/info/default/quickmarks ~/.conkyrc)
+dotfiles=( ~/.vimrc ~/.zshrc ~/.aliasrc ~/.fonts.conf ~/.gitconfig ~/.xbindkeysrc ~/.vimperatorrc ~/.vimperator/info/default/quickmarks ~/.conkyrc ~/.mozilla/firefox/*.default/chrome/userChrome.css ~/.mozilla/firefox/*.default/chrome/userContent.css )
 
 for file in ${dotfiles[*]}
 do
