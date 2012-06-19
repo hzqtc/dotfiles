8797c6831148106ea63a283d4aa42ad012f81ba0
Johnny Huang <hzqtc1229@gmail.com>
2012-06-19 10:29:23 +0800

    exclude firefox css

--- collect.sh
+++ collect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dotfiles=( ~/.vimrc ~/.zshrc ~/.aliasrc ~/.fonts.conf ~/.gitconfig ~/.xbindkeysrc ~/.vimperatorrc ~/.vimperator/info/default/quickmarks ~/.conkyrc ~/.mozilla/firefox/*.default/chrome/userChrome.css ~/.mozilla/firefox/*.default/chrome/userContent.css )
+dotfiles=( ~/.vimrc ~/.zshrc ~/.aliasrc ~/.fonts.conf ~/.gitconfig ~/.xbindkeysrc ~/.vimperatorrc ~/.vimperator/info/default/quickmarks ~/.conkyrc )
 
 for file in ${dotfiles[*]}
 do
